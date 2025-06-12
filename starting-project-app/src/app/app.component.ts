b082bba0aafc9af9601922c7e6ae664ea26bc5df
mszag <zagdan.michal@gmail.com>
2025-06-12 04:54:48 +0000

    updated

--- starting-project-app/src/app/app.component.ts
+++ starting-project-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -10,11 +10,11 @@ import { DUMMY_USERS } from './dummy-users';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
---
+++ starting-project-app/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { type NewTaskData } from './new-task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Output() cancel = new EventEmitter();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
+}
